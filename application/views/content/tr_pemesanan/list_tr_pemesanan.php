ccfc1a011afcefa0089aee35f74a5986751bc4b7
Eddy Christiandy <echristiandy@gmail.com>
2013-09-24 15:15:18 +0700

    Add List Pemesanan

---
+++ application/views/content/tr_pemesanan/list_tr_pemesanan.php
@@ -0,0 +1,16 @@
+<div class="table CSSTabel table-list table-hover" style="top: -45px; width: 25%">
+    <table id="listPemesanan">
+        <thead>
+            <th>Kode PO</th>
+            <th>Tanggal</th>
+            <th></th>
+        </thead>
+    
+        <tbody>
+            <?php foreach($hasil as $row)
+            {
+                
+            } ?>
+        </tbody>
+    </table>
+</div>
