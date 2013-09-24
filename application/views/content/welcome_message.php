bbd1b3dc79170f05b45fa30da78fc8ccebd444dd
Eddy Christiandy <echristiandy@gmail.com>
2013-09-24 21:53:01 +0700

    Add feature bug tracking

--- application/views/content/welcome_message.php
+++ application/views/content/welcome_message.php
@@ -13,7 +13,6 @@
 		
 	</div>
 </div>
-<aside draggable="true" id="dragme">
 <div class="span7" style="margin-left: 0; width: 720px;">
 <div class="bar-span">
 	Summary Chart Sample
@@ -21,10 +20,124 @@
 <div class="content-span" style="height: 205px;">
 	<?php echo $c1;?>
 </div>
-	
-</div>
-</aside>
 
+<div class="content-span" style="height: 190px;">
+<div class="tabbable"> <!-- Only required for left/right tabs -->
+  <ul class="nav nav-tabs">
+    <li class="active"><a href="#tab1" data-toggle="tab">Web Progress</a></li>
+    <li><a href="#tab2" data-toggle="tab">Bug Tracking</a></li>
+  </ul>
+  <div class="tab-content">
+    <div class="tab-pane active" id="tab1">
+        <div id="stat" class="pull-left">
+            <p>Status : Beta Development</p>
+            <p>Current Version : 0.9.24-Beta</p>
+            <p>Date Update : 24/09/2013</p>
+        </div>
+        <div id="prog" class="pull-right">
+            <table width="460px">
+                
+                <tr>
+                    <td width="18%">
+                        HTML
+                    </td>
+                    <td>
+                        <div class="progress progress-warning" style="margin: 0">
+                            <div class="bar" style="width: 60%"></div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="18%">
+                        CSS
+                    </td>
+                    <td>
+                        <div class="progress progress-success" style="margin: 0">
+                            <div class="bar" style="width: 80%"></div>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="18%">
+                        Function
+                    </td>
+                    <td>
+                        <div class="progress progress-danger" style="margin: 0">
+                            <div class="bar" style="width: 50%"></div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+      
+    </div>
+    <div class="tab-pane" id="tab2">
+      <div class="tabbable tabs-right">
+              <ul class="nav nav-tabs">
+                <li class="active"><a href="#rA" data-toggle="tab">Issues</a></li>
+                <li><a href="#rB" data-toggle="tab">Enhance'</a></li>
+                <li><a href="#rC" data-toggle="tab">Report</a></li>
+              </ul>
+              <div class="tab-content">
+                <div class="tab-pane active" id="rA">
+                  <div id="hasil"></div>
+                </div>
+                <div class="tab-pane" id="rB">
+                  <p>Nothing To Enhance at This time</p>
+                </div>
+                <div class="tab-pane" id="rC">
+                    
+                  <form id="formID">
+                      <table width="100%">
+                      <tr>
+                          <td width="20%">
+                              Name*  
+                          </td>
+                          <td width="45%">
+                              <input type="text" placeholder="Name" id="namas">
+                          </td>
+                          <td rowspan="3">
+                              Priority* : <br/>
+                              <label class="radio"><input type="radio" name="optionsRadios" id="optionsRadios1" value="1" checked>
+                              Low
+                              </label>
+                              <label class="radio"><input type="radio" name="optionsRadios" id="optionsRadios1" value="2">
+                              Medium
+                              </label>
+                              <label class="radio"><input type="radio" name="optionsRadios" id="optionsRadios1" value="3">
+                              High
+                              </label>
+                              <button class="btn btn-primary btn-small" id="save">submit</button>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td width="20%">
+                              Title*    
+                          </td>
+                          <td width="45%">
+                              <input type="text" placeholder="Title" id="title">
+                          </td>
+                      </tr>
+                      <tr>
+                          <td width="20%">
+                              Description*  
+                          </td>
+                          <td>
+                              <textarea rows="1" style="height: 50px;" placeholder="Add Info, Description, tips, or url" id="desc"></textarea>
+                          </td>
+                      </tr>
+                    </table>
+                  </form>
+                  
+                </div>
+              </div>
+            </div>
+    </div>
+  </div>
+</div>
+</div>
+
+</div>
 <div class="span3">
 <div class="bar-span">
 	Summary
@@ -71,25 +184,47 @@
 </div>
 
 <script>
-    function drag_start(event) {
-    var style = window.getComputedStyle(event.target, null);
-    event.dataTransfer.setData("text/plain",
-    (parseInt(style.getPropertyValue("left"),10) - event.clientX) + ',' + (parseInt(style.getPropertyValue("top"),10) - event.clientY));
-} 
-function drag_over(event) { 
-    event.preventDefault(); 
-    return false; 
-} 
-function drop(event) { 
-    var offset = event.dataTransfer.getData("text/plain").split(',');
-    var dm = document.getElementById('dragme');
-    dm.style.left = (event.clientX + parseInt(offset[0],10)) + 'px';
-    dm.style.top = (event.clientY + parseInt(offset[1],10)) + 'px';
-    event.preventDefault();
-    return false;
-} 
-var dm = document.getElementById('dragme'); 
-dm.addEventListener('dragstart',drag_start,false); 
-document.body.addEventListener('dragover',drag_over,false); 
-document.body.addEventListener('drop',drop,false); 
-</script>>
+function loadBug(){
+  $.ajax({
+    type:'POST',
+    url: "<?php echo base_url();?>index.php/bug/view",
+    data :{},
+    success:
+    function(hh){
+        $('#hasil').html(hh);
+    }
+    });  
+}
+    
+loadBug();
+$("#save").click(function(){
+        var x = $('input:radio[name=optionsRadios]:checked').val();
+        
+        var nama = $('#namas').val();
+        var title = $('#title').val();
+        var desc = $('#desc').val();
+        var priority = x;
+        
+        if(nama != "" && title != "" && desc != "")
+        {
+            $.ajax({
+            type:'POST',
+            url: "<?php echo base_url();?>index.php/bug/insert", //SEND TO CONTROLLER
+            data :{nama:nama,title:title,desc:desc,priority:priority},
+
+            success:
+            function()
+            {
+                        window.alert("Thanks for Your Feedback and Support");
+                        $('#formID').each(function(){
+                            this.reset();
+                        });
+                        loadBug();
+            }
+            });
+        }else{
+            window.alert("Form tidak boleh kosong");
+        }
+});
+
+</script>
