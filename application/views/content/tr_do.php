95b79720ab18083d10219504969e95dc05da4345
Eddy Christiandy <echristiandy@gmail.com>
2013-10-01 04:24:31 +0700

    Lagi

--- application/views/content/tr_do.php
+++ application/views/content/tr_do.php
@@ -388,7 +388,174 @@ function addRow(tableID) {
         var table = document.getElementById(tableID);
 
         var rowCount = table.rows.length;
+<<<<<<< HEAD
         var row = table.insertRow(rowCount);
+=======
+        var last = rowCount;
+       
+            var table = document.getElementById(tableID);
+ 
+            var rowCount = table.rows.length;
+            var row = table.insertRow(rowCount);
+            
+            //KOLOM 1
+            var cell1 = row.insertCell(0);
+            var iDiv2 = document.createElement("div");
+            iDiv2.className = "input-append";
+            iDiv2.id="kode_brg[]";
+            
+            var element0 = document.createElement("input");
+            element0.type = "text";
+            element0.name="kode_brgd"+last;
+            element0.className="validate[required]";
+            element0.className="span2"; 
+            element0.id="kode_brg"+last;
+            element0.setAttribute("onkeypress", "validAct("+last+")");
+            element0.setAttribute("disable", false);
+            element0.style.width = "70px";
+            
+            var filter = document.createElement("a");
+            filter.className="btn btn-tbl";
+            filter.id="f_brg"+last;
+            filter.href="#myModal2";
+            filter.setAttribute("onclick", "getDetail("+last+")");
+            filter.setAttribute("role", "button");
+            filter.setAttribute("data-toggle", "modal");
+            var iIcon3 = document.createElement("i");
+            iIcon3.className = "icon-filter";
+            
+            filter.appendChild(iIcon3);
+            iDiv2.appendChild(element0);
+            iDiv2.appendChild(filter);
+            cell1.appendChild(iDiv2);
+
+            //KOLOM 2
+            var cell1 = row.insertCell(1);
+            var element1 = document.createElement("input");
+            element1.type = "text";
+            element1.name = "qty_brg";
+            element1.id="qty_brg"+last;
+            element1.setAttribute("onkeypress", "validAct("+last+")");
+            element1.style.width = "30px";
+            cell1.appendChild(element1);
+            
+            //KOLOM 3
+            var cell2 = row.insertCell(2);
+            var element2 = document.createElement("input");
+            element2.type = "text";
+            element2.name = "satuan_brg[]";
+            element2.disabled = "true";
+            element2.id="satuan_brg"+last;
+            element2.style.width = "70px";
+            cell2.appendChild(element2);
+            
+            //KOLOM 4
+            var cell3 = row.insertCell(3);
+            var element3 = document.createElement("input");
+            element3.type = "text";
+            element3.name = "harga[]";
+            element3.id="harga_brg"+last;
+            element0.setAttribute("onkeypress", "validAct("+last+")");
+            element3.style.width = "70px";
+            cell3.appendChild(element3);
+            //KOLOM 5
+            var cell4 = row.insertCell(4);
+            var element4 = document.createElement("input");
+            element4.type = "text";
+            element4.name = "jumlah";
+            element4.id="jumlah_brg"+last;
+            element4.style.width = "70px";
+            element4.disabled = "true";
+            cell4.appendChild(element4);
+            //KOLOM 6
+            var cell5 = row.insertCell(5);
+            var element5 = document.createElement("input");
+            element5.type = "text";
+            element5.name = "keterangan[]";
+            element5.id="keterangan_brg"+last;
+            element5.style.width = "80px";
+            cell5.appendChild(element5);
+            //KOLOM 7
+            var cell6 = row.insertCell(6);
+                        
+            var iIcon1 = document.createElement("i");
+            iIcon1.className = "icon-ok";
+            iIcon1.id="icon"+last;
+            var iIcon2 = document.createElement("i");
+            iIcon2.className = "icon-trash";
+            
+            var iAnchor1 = document.createElement("a");
+            iAnchor1.className = "btn";
+            iAnchor1.href = "#";
+            iAnchor1.setAttribute('onclick', 'editRow('+last+')')
+            iAnchor1.appendChild(iIcon1);
+            var iAnchor2 = document.createElement("a");
+            iAnchor2.className = "btn";
+            iAnchor2.setAttribute('onclick', 'deleteRowSO(this)')
+            iAnchor2.href = "#";
+            iAnchor2.appendChild(iIcon2);
+            
+            cell6.appendChild(iAnchor1);
+            cell6.appendChild(iAnchor2);
+        }
+        
+		//BUAT PRINT
+ $("#print").click(function(){
+	//deklarasi variable
+        var so = $('#_so').val();
+        var tglSo = $('#_tgl').val();
+        var po = $('#_po').val();
+        var tglPo = $('#_tgl2').val();
+        var pl = $('#kd_plg').val();
+        var sl = $('#_sl').val();
+        var to = $('#total').val();
+        
+        var arrKode = new Array();
+        var arrQty = new Array();
+		var arrSatuan = new Array();
+        var arrHarga = new Array();
+        var arrJumlah = new Array();
+        var arrKet = new Array();
+        
+        var table = document.getElementById('tb3');
+        var totalRow = table.rows.length-1;
+        for(var i=1;i<=totalRow;i++){
+			arrKode[i-1] = $('#kode_brg'+i).val();
+			arrQty[i-1] = $('#qty_brg'+i).val();
+			arrSatuan[i-1] = $('#satuan_brg'+i).val();
+			arrHarga[i-1] = $('#harga_brg'+i).val();
+			arrJumlah[i-1] = $('#jumlah_brg'+i).val();
+			arrKet[i-1] = $('#keterangan_brg'+i).val();	
+		}	
+        
+        
+	$.ajax({
+	        type:'POST',
+	        url: "<?php echo base_url();?>index.php/report/print_transaksi_so",
+	        data :{ so:so,tglSo:tglSo,po:po,tglPo:tglPo,pl:pl,sl:sl,to:to,
+	                    arrKode:arrKode, arrQty:arrQty, arrSatuan:arrSatuan, arrHarga:arrHarga, arrJumlah:arrJumlah, arrKet:arrKet,totalRow:totalRow
+	        },
+	
+	        success:
+	        function(msg)
+	        {	
+				var win=window.open('about:blank');
+				with(win.document)
+				{
+				  open();
+				  write(msg);
+				  close();
+				}
+	        }
+	     });
+});
+		
+		
+    //Save Click
+    $("#save").click(function(){
+    	
+        var mode = $('#save').attr("mode");
+>>>>>>> bc5f7597b2598924438ce99f0a458ab271d8700a
         
 <<<<<<< HEAD
         //KOLOM 1
