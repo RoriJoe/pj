5b2bc096d6d29e0a4f93eddaf8cd075d3abb0e8e
Eddy Christiandy <echristiandy@gmail.com>
2013-11-08 17:35:18 +0700

    fix table detail invoice

--- application/views/content/tr_invoice/detail_invoice.php
+++ application/views/content/tr_invoice/detail_invoice.php
@@ -1,13 +1,13 @@
 <div class="table CSSTabel" style="margin-bottom:5px;">
 <table id="tb_detail">
     <thead>
-        <th>Kode</th>
-        <th>Nama Barang</th>
-        <th>Satuan</th>
-        <th>Qty</th>
-        <th>Hrg Satuan (Rp)</th>
-        <th>Jumlah (Rp)</th>
-        <th>Keterangan</th>
+        <th width="10%">Kode</th>
+        <th width="30%">Nama Barang</th>
+        <th width="10%">Satuan</th>
+        <th width="10%">Qty</th>
+        <th width="15%">Hrg Satuan (Rp)</th>
+        <th width="15%">Jumlah (Rp)</th>
+        <th width="10%">Keterangan</th>
     </thead>
     <tbody id="itemlist">
     <?php
@@ -18,25 +18,25 @@
         $qty = $row->Qty1;
         $Jumlah = number_format($row->Harga*$qty, 0, ',', '.');
         echo "<tr>
-        <td>
+        <td width='10%'>
             <label id='kd$i'>$row->Kode_Brg</label>
         </td>
-        <td>
+        <td width='30%'>
             <label id='brg$i'>$row->Barang $row->Ukuran </label>
         </td>
-        <td>
+        <td width='10%'>
             <label id='satuan$i'>$row->Satuan1</label>
         </td>
-        <td>
+        <td width='10%'>
             <label id='qty$i'>$row->Qty1</label>
         </td>
-        <td>
+        <td width='15%'>
             <input type='text' name='harga_brg' class='validate[required]' id='harga_brg$i' style='width:88px; text-align:right;' value='$Harga' readonly='true'/>
         </td>
-        <td>
+        <td width='15%'>
             <input type='text' name='jumlah' class='validate[required]' id='jumlah_brg$i' style='width:88px;text-align:right;' value='$Jumlah' disabled='true'/>
         </td>
-        <td>
+        <td width='10%'>
             <label id='ket$i'>$row->Keterangan</label>
         </td>
         </tr>
@@ -51,10 +51,9 @@
 var oTable = $('#tb_detail').dataTable( {
     "sScrollY": "130px",
     "sScrollYInner": "100%",
-    "sScrollX": "100%", //panjang width
-    "sScrollXInner": "100%", //overflow dalem
     "bPaginate": false,
     "bLengthChange": false,
+    "bAutoWidth":false,
     "bFilter": false,
     "bInfo": false //Showing 1 to 1 of 1 entries (filtered from 7 total entries)
 } );
